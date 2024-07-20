e0741ea10ef2981bcbe80703bc4af916dac726f4
qoaqq <129212733+qoaqq@users.noreply.github.com>
2024-07-20 23:21:37 +0700

    booking-type by quangduoc

---
+++ src/app/services/booking-type/booking-type.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { BookingTypeService } from './booking-type.service';
+
+describe('BookingTypeService', () => {
+  let service: BookingTypeService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(BookingTypeService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
