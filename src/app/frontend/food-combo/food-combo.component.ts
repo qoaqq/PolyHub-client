1f0f06393feb58549372922fd24d6db6c8a6db4a
thetruong30102002 <thetruongnguyen30102002@gmail.com>
2024-07-26 17:54:00 +0700

    update foodcombo, bookingtype

--- src/app/frontend/food-combo/food-combo.component.ts
+++ src/app/frontend/food-combo/food-combo.component.ts
@@ -1,36 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { Router,ActivatedRoute  } from '@angular/router';
-import { FoodComboService } from 'src/app/services/food-combo/food-combo.service';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-food-combo',
   templateUrl: './food-combo.component.html',
   styleUrls: ['./food-combo.component.scss']
 })
-export class FoodComboComponent implements OnInit {
-  foodCombos: any[] = [];
-  selectedSeats: any[] = [];
+export class FoodComboComponent{
+  @Input() foodCombos: any[] = [];
+  @Output() quantityChange = new EventEmitter<{ combo: any; quantity: number }>();
 
-  constructor(
-    private foodComboService: FoodComboService,
-    private router: Router,
-    private route: ActivatedRoute
-  ) { }
-
-  ngOnInit(): void {
-    
-    this.loadFoodCombos();
+  onQuantityChange(combo: any, quantity: number): void {
+    this.quantityChange.emit({ combo, quantity });
   }
-
-  loadFoodCombos(): void {
-    this.foodComboService.getFoodCombos().subscribe(
-      (data) => {
-        this.foodCombos = data.data.data;
-        console.log('Food Combos:', this.foodCombos);
-      },
-      (error) => {
-        console.error('Error fetching food combos:', error);
-      }
-    );
-}
 }
